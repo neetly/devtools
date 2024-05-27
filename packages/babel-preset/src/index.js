290c9061f20ec7415112419d5d9277e020b87bec
Hikari Hayashi <rev.hikari@gmail.com>
2024-05-27 22:26:45 +0800

    Use esm

--- packages/babel-preset/src/index.js
+++ packages/babel-preset/src/index.js
@@ -3,7 +3,7 @@ import { declarePreset } from "@babel/helper-plugin-utils";
 export default declarePreset(() => {
   return {
     presets: [
-      import.meta.resolve("@babel/preset-env"),
+      [import.meta.resolve("@babel/preset-env"), { modules: false }],
       import.meta.resolve("@babel/preset-react"),
       [
         import.meta.resolve("@babel/preset-typescript"),
