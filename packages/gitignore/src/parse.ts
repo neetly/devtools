e8b6f9b69994d73dd83407fa6956884d3ef0f91f
Hikari Hayashi <rev.hikari@gmail.com>
2024-05-28 00:30:32 +0800

    Add gitignore package

---
+++ packages/gitignore/src/parse.ts
@@ -0,0 +1,31 @@
+export const parse = (content: string) => {
+  const patterns = content
+    .split("\n")
+
+    // Remove comments
+    .filter((pattern) => !pattern.startsWith("#"))
+    .map((pattern) => pattern.replace(/^\\#/, "#"))
+
+    // Remove trailing spaces and empty lines
+    .map((pattern) => pattern.replace(/(?<!\\) +$/, "").replace(/\\ $/, " "))
+    .filter((pattern) => pattern !== "")
+
+    // Remove non-sense patterns
+    .filter((pattern) => !/^!?\/?$/.test(pattern))
+
+    // Normalize slashes
+    .map((pattern) => {
+      const isNegative = pattern.startsWith("!");
+      let path = pattern.replace(/^!/, "");
+      if (!/^\/|\/(?!$)/.test(path)) {
+        path = "**/" + path;
+      }
+      path = path.replace(/^\//, "");
+      return (isNegative ? "!" : "") + path;
+    })
+
+    // Escape special characters
+    .map((pattern) => pattern.replace(/[(){}\\]/g, "\\$&"));
+
+  return { patterns };
+};
