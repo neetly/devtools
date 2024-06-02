0fc5ba100a3843e5c3864bf4539a6c62012a7633
Hikari Hayashi <rev.hikari@gmail.com>
2024-06-02 22:53:06 +0800

    Fix lint error

--- packages/eslint-config/src/eslint-plugin-unicorn.d.ts
+++ packages/eslint-config/src/eslint-plugin-unicorn.d.ts
@@ -1,2 +1,6 @@
 // FIXME: https://github.com/sindresorhus/eslint-plugin-unicorn/issues/2324
-declare module "eslint-plugin-unicorn";
+declare module "eslint-plugin-unicorn" {
+  import type { ESLint } from "eslint";
+  const plugin: ESLint.Plugin;
+  export default plugin;
+}
