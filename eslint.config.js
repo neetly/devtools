5199fc747332bda633a7673cd0d6c8e91d3c7b10
Hikari Hayashi <rev.hikari@gmail.com>
2024-06-03 01:47:43 +0800

    Workaround vscode-eslint issue

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,10 @@
+import process from "node:process";
+
 import { createConfig } from "@neetly/eslint-config";
 
 export default createConfig({
-  rootDir: import.meta.dirname,
+  rootDir:
+    import.meta.dirname ??
+    // FIXME: https://github.com/microsoft/vscode-eslint/issues/1851
+    process.cwd(),
 });
