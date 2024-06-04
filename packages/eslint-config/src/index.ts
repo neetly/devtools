40835f8db163b13f9e3729388f412e2e330b28d3
Hikari Hayashi <rev.hikari@gmail.com>
2024-06-05 00:27:40 +0800

    Do not throw if .gitignore does not exist

--- packages/eslint-config/src/index.ts
+++ packages/eslint-config/src/index.ts
@@ -9,13 +9,20 @@ import node from "./configs/node.js";
 import react from "./configs/react.js";
 
 export const createConfig = async ({ rootDir }: { rootDir: string }) => {
+  let ignores: string[];
+  try {
+    ignores = gitignore.parse(
+      await fs.readFile(path.join(rootDir, ".gitignore"), {
+        encoding: "utf8",
+      }),
+    ).patterns;
+  } catch {
+    ignores = [];
+  }
+
   return tseslint.config(
     {
-      ignores: gitignore.parse(
-        await fs.readFile(path.join(rootDir, ".gitignore"), {
-          encoding: "utf8",
-        }),
-      ).patterns,
+      ignores,
     },
 
     ...base,
