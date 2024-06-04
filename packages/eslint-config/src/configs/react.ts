cf051ef1433547b8562cbbc4cb65c4d0979ca568
Hikari Hayashi <rev.hikari@gmail.com>
2024-06-05 00:03:03 +0800

    Add @eslint-react

--- packages/eslint-config/src/configs/react.ts
+++ packages/eslint-config/src/configs/react.ts
@@ -1,12 +1,25 @@
+import eslintPluginReact from "@neetly-eslint-react/eslint-plugin";
 import eslintPluginReactHooks from "eslint-plugin-react-hooks";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config({
-  plugins: {
-    "react-hooks": eslintPluginReactHooks,
+export default tseslint.config(
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    extends: [eslintPluginReact.configs.recommended],
   },
-  rules: {
-    // FIXME: https://github.com/facebook/react/issues/28313
-    ...eslintPluginReactHooks.configs.recommended.rules,
+
+  {
+    files: ["**/*.{ts,tsx,mts,cts}"],
+    extends: [eslintPluginReact.configs["recommended-type-checked"]],
   },
-});
+
+  {
+    plugins: {
+      "react-hooks": eslintPluginReactHooks,
+    },
+    rules: {
+      // FIXME: https://github.com/facebook/react/issues/28313
+      ...eslintPluginReactHooks.configs.recommended.rules,
+    },
+  },
+);
