b40f4608310abdde681212c5512bc7f07f7836c6
Hikari Hayashi <rev.hikari@gmail.com>
2024-06-15 22:14:39 +0800

    Use system Node.js for vscode-eslint; remove workaround code

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,5 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
 import { createConfig } from "@neetly/eslint-config";
 
 export default createConfig({
-  rootDir:
-    import.meta.dirname ??
-    // FIXME: https://github.com/microsoft/vscode-eslint/issues/1851
-    path.dirname(fileURLToPath(import.meta.url)),
+  rootDir: import.meta.dirname,
 });
