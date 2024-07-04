d57c224206e5907cad9501c491ad755611a6668d
Hikari Hayashi <rev.hikari@gmail.com>
2024-07-05 01:42:56 +0800

    Fix jest config

--- packages/jest-config/src/index.ts
+++ packages/jest-config/src/index.ts
@@ -3,7 +3,7 @@ import { fileURLToPath } from "node:url";
 import * as babelUtils from "@neetly/babel-utils";
 import type { Config } from "jest";
 
-export const createConfig = (): Config => {
+export const createConfig = async (): Promise<Config> => {
   return {
     injectGlobals: false,
 
@@ -16,7 +16,7 @@ export const createConfig = (): Config => {
       "\\.(?:js|mjs|cjs|ts|tsx|mts|cts)$": [
         // FIXME: https://github.com/jestjs/jest/pull/15154
         fileURLToPath(import.meta.resolve("babel-jest")),
-        babelUtils.createOptions({
+        await babelUtils.createOptions({
           defaultPresets: [import.meta.resolve("@neetly/babel-preset")],
         }),
       ],
