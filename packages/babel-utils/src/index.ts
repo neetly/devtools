e691b9cc4d6c25c31c978bda0312613916c094db
Hikari Hayashi <rev.hikari@gmail.com>
2024-07-05 00:57:08 +0800

    Add babel-utils package

---
+++ packages/babel-utils/src/index.ts
@@ -0,0 +1,37 @@
+import * as babel from "@babel/core";
+
+export const hasCustomConfig = async (): Promise<boolean> => {
+  const { config } = await babel.loadPartialConfigAsync({
+    babelrc: false,
+    showIgnoredFiles: true,
+  });
+  return config !== undefined;
+};
+
+export const createOptions = async ({
+  defaultPresets = [],
+  defaultPlugins = [],
+  extraPresets = [],
+  extraPlugins = [],
+  ...options
+}: Omit<babel.InputOptions, "presets" | "plugins"> & {
+  defaultPresets?: babel.InputOptions["presets"];
+  defaultPlugins?: babel.InputOptions["plugins"];
+  extraPresets?: babel.InputOptions["presets"];
+  extraPlugins?: babel.InputOptions["plugins"];
+} = {}): Promise<babel.InputOptions> => {
+  const useDefaultPresetsAndPlugins = !(await hasCustomConfig());
+
+  return {
+    babelrc: false,
+    presets: [
+      ...(useDefaultPresetsAndPlugins ? [] : defaultPresets),
+      ...extraPresets,
+    ],
+    plugins: [
+      ...(useDefaultPresetsAndPlugins ? [] : defaultPlugins),
+      ...extraPlugins,
+    ],
+    ...options,
+  };
+};
--- packages/jest-config/src/index.ts
+++ packages/jest-config/src/index.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath } from "node:url";
 
+import * as babelUtils from "@neetly/babel-utils";
 import type { Config } from "jest";
 
 export const createConfig = (): Config => {
@@ -15,7 +16,9 @@ export const createConfig = (): Config => {
       "\\.(?:js|mjs|cjs|ts|tsx|mts|cts)$": [
         // FIXME: https://github.com/jestjs/jest/pull/15154
         fileURLToPath(import.meta.resolve("babel-jest")),
-        { presets: [import.meta.resolve("@neetly/babel-preset")] },
+        babelUtils.createOptions({
+          defaultPresets: [import.meta.resolve("@neetly/babel-preset")],
+        }),
       ],
     },
   };
