98b9696ea8f5b86855b54f8881b43bfd78d40c5f
Hikari Hayashi <rev.hikari@gmail.com>
2024-07-05 00:02:56 +0800

    Add babel-jest

--- packages/jest-config/src/index.ts
+++ packages/jest-config/src/index.ts
@@ -1,3 +1,5 @@
+import { fileURLToPath } from "node:url";
+
 import type { Config } from "jest";
 
 export const createConfig = (): Config => {
@@ -10,7 +12,11 @@ export const createConfig = (): Config => {
     extensionsToTreatAsEsm: [".ts", ".tsx", ".mts"],
 
     transform: {
-      // TODO
+      "\\.(?:js|mjs|cjs|ts|tsx|mts|cts)$": [
+        // FIXME: https://github.com/jestjs/jest/pull/15154
+        fileURLToPath(import.meta.resolve("babel-jest")),
+        { presets: [import.meta.resolve("@neetly/babel-preset")] },
+      ],
     },
   };
 };
