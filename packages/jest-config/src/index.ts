8bd8fb9999b8285b6e34ab7207576c6bd85517d8
Hikari Hayashi <rev.hikari@gmail.com>
2024-07-05 01:45:08 +0800

    Update some comments

--- packages/jest-config/src/index.ts
+++ packages/jest-config/src/index.ts
@@ -14,7 +14,7 @@ export const createConfig = async (): Promise<Config> => {
 
     transform: {
       "\\.(?:js|mjs|cjs|ts|tsx|mts|cts)$": [
-        // FIXME: https://github.com/jestjs/jest/pull/15154
+        // FIXME: https://github.com/jestjs/jest/issues/15153
         fileURLToPath(import.meta.resolve("babel-jest")),
         await babelUtils.createOptions({
           defaultPresets: [import.meta.resolve("@neetly/babel-preset")],
