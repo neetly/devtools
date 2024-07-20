45f9b006b89adb16626a83f48501b114d5a8ac1f
Hikari Hayashi <rev.hikari@gmail.com>
2024-07-21 03:34:40 +0800

    [gitignore] Spec compliance

--- packages/gitignore/src/parse.ts
+++ packages/gitignore/src/parse.ts
@@ -1,28 +1,36 @@
 export const parse = (content: string): { patterns: readonly string[] } => {
   const patterns = content
+
+    // Remove BOM
     .replace(/^\uFEFF/, "")
+
+    // Split by LF or CRLF
     .split(/\r?\n/)
 
     // Remove comments
     .filter((pattern) => !pattern.startsWith("#"))
     .map((pattern) => pattern.replace(/^\\#/, "#"))
 
-    // Remove trailing spaces and empty lines
+    // Remove trailing spaces
     .map((pattern) => pattern.replace(/(?<!\\) +$/, "").replace(/\\ $/, " "))
+
+    // Remove empty patterns
     .filter((pattern) => pattern !== "")
 
     // Remove non-sense patterns
     .filter((pattern) => !/^!?\/?$/.test(pattern))
 
-    // Normalize slashes
+    // Normalize slashes ("/") and two consecutive asterisks ("**")
     .map((pattern) => {
       const isNegative = pattern.startsWith("!");
-      let path = pattern.replace(/^!/, "");
-      if (!/^\/|\/(?!$)/.test(path)) {
+      const isDirectory = pattern.endsWith("/");
+      let path = pattern.replace(/^!/, "").replace(/\/$/, "");
+      if (!path.includes("/")) {
         path = "**/" + path;
       }
       path = path.replace(/^\//, "");
-      return (isNegative ? "!" : "") + path;
+      path = path.replace(/(?<=^|\/)\*\*$/, "**/*");
+      return (isNegative ? "!" : "") + path + (isDirectory ? "/" : "");
     })
 
     // Escape special characters
